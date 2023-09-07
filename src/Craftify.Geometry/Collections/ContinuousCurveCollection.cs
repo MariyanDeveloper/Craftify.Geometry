b7a4b0dd9d1c2e96a825fe40c94c122cc4e0e0e5
MariyanSlesar <marianslesar.08@gmail.com>
2023-09-07 10:55:15 +0300

    Add Profile and ContinuousCurveCollection collections

---
+++ src/Craftify.Geometry/Collections/ContinuousCurveCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace Craftify.Geometry.Collections;
+
+public class ContinuousCurveCollection : List<Curve>
+{
+    public ContinuousCurveCollection() { }
+    public ContinuousCurveCollection(IEnumerable<Curve> curves) : base(curves)
+    {
+        
+    }
+}
---
+++ src/Craftify.Geometry/Collections/Profile.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using Craftify.Geometry.Extensions;
+using Craftify.Geometry.Extensions.Curves;
+
+namespace Craftify.Geometry.Collections;
+
+public class Profile : List<CurveLoop>, IEquatable<Profile>
+{
+    public Profile(IEnumerable<CurveLoop> curveLoops): base(curveLoops) { }
+    public bool Equals(Profile? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+        var flattenCurrentCurves = this.SelectMany(x => x);
+        var flattenOtherCurves = other.SelectMany(x => x);
+        return flattenCurrentCurves
+            .All(expected => flattenOtherCurves.Any(expected.CompletelyInside));
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj))
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (obj.GetType() != GetType())
+        {
+            return false;
+        }
+        return Equals((Profile)obj);
+    }
+    public static bool operator ==(Profile? left, Profile? right)
+    {
+        if (ReferenceEquals(left, null))
+        {
+            return ReferenceEquals(right, null);
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Profile? left, Profile? right)
+    {
+        return !(left == right);
+    }
+    public override int GetHashCode()
+    {
+        //TODO come up with a meaningful hash code
+        return 1;
+    }
+}
