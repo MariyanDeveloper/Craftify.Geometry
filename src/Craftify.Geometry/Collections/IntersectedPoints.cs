f09f3b683c2d12a630a92346ae6715ced58858c9
MariyanSlesar <marianslesar.08@gmail.com>
2023-09-07 11:36:56 +0300

    Add IntersectedPoints.cs collection

---
+++ src/Craftify.Geometry/Collections/IntersectedPoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace Craftify.Geometry.Collections;
+
+public class IntersectedPoints : List<XYZ>
+{
+    public IntersectedPoints() { }
+    public IntersectedPoints(IEnumerable<XYZ> points): base(points)
+    {
+        
+    }
+}
